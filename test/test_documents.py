985e32b392e70d01e4091d4e36c5cd32ef1cb184
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-20 20:30:34 -0500

    move tests to test folder

---
+++ test/test_documents.py
@@ -0,0 +1,42 @@
+from unittest import TestCase, skip
+from inspect import isgenerator
+
+from python_document import Document, Documents
+
+
+class TestDocuments(TestCase):
+
+    test_data = [
+        { 'alpha': 'a' },
+        { 'beta': 'b' },
+        { 'gamma': 'g' },
+    ]
+
+    def setUp(self):
+        self.docs = Documents(self.test_data)
+
+    def test_init(self):
+        map(self.assertDictEqual, self.docs, self.test_data)
+
+    def test_getitem(self):
+        docs = Documents(self.test_data)
+        self.assertDictEqual(docs[0], self.test_data[0])
+
+    @skip('not tested')
+    def test_getslice(self):
+        pass
+
+    def test_iter(self):
+        gen = iter(self.docs)
+        self.assertTrue(isgenerator(gen))
+        self.assertTrue(isinstance(next(gen), Document))
+
+    #@skip('not tested')
+    def test_pop(self):
+        value = self.docs.pop()
+        self.assertEqual(value, self.test_data[len(self.test_data) - 1])
+        value = self.docs.pop(0)
+        self.assertEqual(value, self.test_data[0])
+        self.docs.pop()
+        with self.assertRaises(IndexError):
+            self.docs.pop()
---
+++ test/test_typecheck.py
@@ -0,0 +1,24 @@
+from unittest import TestCase
+
+from python_document import typecheck, Document, Documents
+
+
+class TestTypecheck(TestCase):
+
+    test_data = [
+        { 'alpha': 'a' },
+        { 'beta': 'b' },
+        { 'gamma': 'g' },
+    ]
+
+    def setUp(self):
+        self.docs = typecheck(self.test_data)
+
+    def test_base(self):
+        self.assertTrue(isinstance(self.docs[0]['alpha'], str))
+
+    def test_dict(self):
+        self.assertTrue(isinstance(self.docs[0], Document))
+
+    def test_list(self):
+        self.assertTrue(isinstance(self.docs, Documents))

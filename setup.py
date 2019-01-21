1cb49c032124909257bbd2f97967448fb3787e7f
Lucifer Avada <lucifer.avada@gmail.com>
2019-01-20 20:42:27 -0500

    add setup.py

---
+++ setup.py
@@ -0,0 +1,14 @@
+__author__ = 'Lucifer Avada'
+
+from setuptools import setup
+
+setup(
+    name='sugar-document',
+    version='0.0.1',
+    author='Lucifer Avada',
+    author_email='lucifer.avada@gmail.com',
+    url='https://github.com/sugarush/sugar-document',
+    packages=['sugar_document'],
+    description='Map dictionary keys to object attributes.',
+    install_requires=[ ]
+)
